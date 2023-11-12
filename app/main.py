cfff28ba229ea6a3ffe4c898267a44f5c0949eb0
payashi <6phyphy6@gmail.com>
2023-11-12 10:35:14 +0900

    Upload to GCS

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@
 
 import logging
 
-
 import os
+import base64
 from datetime import datetime
 
 import firebase_admin
 from firebase_admin import firestore, credentials
+
+from google.cloud import storage
 
 from flask import Flask, request, jsonify
 from werkzeug.exceptions import BadRequest
@@ -19,7 +21,12 @@ app = Flask(__name__)
 cred = credentials.ApplicationDefault()
 
 firebase_admin.initialize_app(cred)
-db = firestore.client()
+firestore_client = firestore.client()
+
+gcs_client = storage.Client()
+bucket = gcs_client.get_bucket("duck-audio")
+if not bucket.exists():
+    logging.error("There's no GCS Bucket to upload to")
 
 logging.basicConfig(
     format="%(asctime)s:%(message)s",
@@ -29,7 +36,7 @@ logging.basicConfig(
 
 
 @app.route("/", methods=["POST"])
-def index():
+def upload():
     """JSON test"""
     try:
         data = request.get_json()
@@ -40,7 +47,8 @@ def index():
         user_id=data["user_id"],
         duck_id=data["duck_id"],
     )
-    return f"Server: Got {data['user_id']}"
+    upload_audio(remark_id, data["audio"])
+    return f"Remark #{remark_id} was successfully uploaded"
 
 
 def upload_remark(user_id: str, duck_id: str) -> str:
@@ -51,11 +59,24 @@ def upload_remark(user_id: str, duck_id: str) -> str:
         "duck_id": duck_id,
         "created_at": now.timestamp(),
     }
-    _, ref = db.collection("remarks").add(remark)
+    _, ref = firestore_client.collection("remarks").add(remark)
     logging.info(
         "Added document remarks/%s at %s", ref.id, now.strftime("%Y-%m-%d %H:%M:%S")
     )
     return ref.id
+
+
+def upload_audio(remark_id: str, data: str):
+    """Upload audio data to Cloud Storage"""
+    blob = bucket.blob(f"audio/{remark_id}.wav")
+    blob.upload_from_string(
+        data=_unmarshal(data),
+        content_type="audio/wav",
+    )
+
+
+def _unmarshal(data: str) -> bytes:
+    return base64.b64decode(data.encode("utf-8"))
 
 
 if __name__ == "__main__":
