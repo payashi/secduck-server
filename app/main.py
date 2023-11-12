6e0d16bcf6c72de904222866cda68a83188dd912
payashi <6phyphy6@gmail.com>
2023-11-12 09:03:53 +0900

    Upload remark to firestore

--- app/main.py
+++ app/main.py
@@ -1,28 +1,64 @@
 #!/usr/bin/env python
 
-import json
-import base64
-import wave
+import logging
 
 
 import os
+from datetime import datetime
+
+import firebase_admin
+from firebase_admin import firestore, credentials
 
 from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
+
+
+# Use the application default credentials.
+cred = credentials.ApplicationDefault()
+
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+logging.basicConfig(
+    format="%(asctime)s:%(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.DEBUG,
+)
 
 
 @app.route("/", methods=["POST"])
 def index():
     """JSON test"""
-    data = request.get_json()
-    print(jsonify(data))
+    try:
+        data = request.get_json()
+    except BadRequest:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    remark_id = upload_remark(
+        user_id=data["user_id"],
+        duck_id=data["duck_id"],
+    )
     return f"Server: Got {data['user_id']}"
 
 
-if __name__ == "__main__":
-    PORT = int(os.environ.get("PORT", 8080))
+def upload_remark(user_id: str, duck_id: str) -> str:
+    """Upload remark object to firestore and returns a new id"""
+    now = datetime.now()
+    remark = {
+        "user_id": user_id,
+        "duck_id": duck_id,
+        "created_at": now.timestamp(),
+    }
+    _, ref = db.collection("remarks").add(remark)
+    logging.info(
+        "Added document remarks/%s at %s", ref.id, now.strftime("%Y-%m-%d %H:%M:%S")
+    )
+    return ref.id
 
+
+if __name__ == "__main__":
     # This is used when running locally. Gunicorn is used to run the
     # application on Cloud Run. See entrypoint in Dockerfile.
-    app.run(host="localhost", port=PORT, debug=True)
+    app.run(host="localhost", port=int(os.environ.get("PORT", 8080)), debug=True)
